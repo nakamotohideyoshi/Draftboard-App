7ef1b3164422faa74d39b61989c8099057472c64
karloscarweber <me@kow.fm>
2015-09-17 11:25:21 -0600

    Add ContestsDetailViewController Stub.

---
+++ Experiments/RedMesa/RedMesa/ContestsDetailViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ContestsDetailViewController.swift
+//  RedMesa
+//
+//  Created by Karl Weber on 9/17/15.
+//  Copyright © 2015 Rally Interactive. All rights reserved.
+//
+
+import UIKit
+
+class ContestsDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    // add fancy views to the thing.
+    func setupData() {
+    
+    
+    }
+
+}
