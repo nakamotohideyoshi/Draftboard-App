daff2088f46539ca5c6f7c1c1413c084df5e0ba8
Geof Crowl <geof@rallyinteractive.com>
2015-10-30 12:34:52 -0600

    Create GlobalFilterViewController nib and class
    * Crude layout adjustments made in class, feel free to change
    * Height of filter is based off of label height in combination
      with a multiplier

--- Draftboard/Draftboard/UI/GlobalFilterViewController.swift
+++ Draftboard/Draftboard/UI/GlobalFilterViewController.swift
@@ -8,28 +8,73 @@
 
 import UIKit
 
-class GlobalFilterViewController: UIViewController {
+class GlobalFilterViewController: DraftboardViewController {
 
+    @IBOutlet var allLabel: DraftboardLabel!
+    @IBOutlet var mlbLabel: DraftboardLabel!
+    @IBOutlet var nbaLabel: DraftboardLabel!
+    @IBOutlet var nflLabel: DraftboardLabel!
+    
+    // the textContainer that contains all labels
+    @IBOutlet var textContainer: UIView!
+    @IBOutlet var containerHeight: NSLayoutConstraint!
+    
+    // all will always be present, let's reference it's height
+    @IBOutlet var allLabelHeight: NSLayoutConstraint!
+    
+    var filterItems = [DraftboardLabel]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        filterItems = [
+            allLabel,
+            mlbLabel,
+            nbaLabel,
+            nflLabel
+        ]
+        
+        // items that are selected are whiteColor
+        allLabel.textColor = UIColor.whiteColor()
+        
+        // items that aren't selected get whiteLowOpacity
+        mlbLabel.textColor = UIColor.whiteLowOpacity()
+        nbaLabel.textColor = UIColor.whiteLowOpacity()
+        nflLabel.textColor = UIColor.whiteLowOpacity()
+        
+        // the height of textContainer needs to be a multiple of the items
+        // that are inside of it for it to center correctly
+        let heightMultiplier = CGFloat(filterItems.count)
+        containerHeight = textContainer.heightRancor
+            .constraintEqualToRancor(allLabel.heightRancor, multiplier: heightMultiplier, constant: 4)
+        containerHeight.active = true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func didTapTitlebarButton(buttonType: TitlebarButtonType) {
+        if (buttonType == .Close) {
+            self.navController?.popViewController()
+        }
     }
-    */
+    
+    override func titlebarTitle() -> String? {
+        // The global filters view doesn't have a title
+        return ""
+    }
+    
+    override func titlebarLeftButtonType() -> TitlebarButtonType? {
+        return .Close
+    }
+}
 
+@IBDesignable
+class FilterLabel: DraftboardLabel {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.textColor = UIColor.whiteLowOpacity()
+        self.transform = CGAffineTransformMakeRotation(CGFloat(M_PI / 2))
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
