e77b618676cec413ee9a36fe97587a8e71924430
Geof Crowl <geof@rallyinteractive.com>
2015-11-04 15:26:00 -0700

    Update visual appearance of lineup card via nib

--- Draftboard/Draftboard/Lineup/LineupCardView.swift
+++ Draftboard/Draftboard/Lineup/LineupCardView.swift
@@ -18,6 +18,10 @@ class LineupCardView: DraftboardNibView {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var headerView: UIView!
     @IBOutlet weak var contentView: UIScrollView!
+    @IBOutlet weak var contentViewContainer: UIView!
+    @IBOutlet weak var dividerHeight: NSLayoutConstraint!
+    @IBOutlet weak var containerDividerHeight: NSLayoutConstraint!
+    @IBOutlet weak var pmrGraph: UIView!
     
 //    let itemCount = Int(arc4random_uniform(12)) + 1
     let itemCount = 12
@@ -26,6 +30,22 @@ class LineupCardView: DraftboardNibView {
     
     override func willAwakeFromNib() {
         contentView.indicatorStyle = .White
+        contentView.alwaysBounceVertical = true
+
+        contentViewContainer.clipsToBounds = true
+        contentView.clipsToBounds = false
+        
+        // set dividers to real 1px height
+        let onePixel = 1 / UIScreen.mainScreen().scale
+        dividerHeight.constant = onePixel
+        containerDividerHeight.constant = onePixel
+        
+//        UIGraphicsBeginImageContextWithOptions(contentView.bounds.size, true, 1)
+//        drawViewHierarchyInRect(contentView.bounds, afterScreenUpdates: true)
+//        let screenshot = UIGraphicsGetImageFromCurrentImageContext()
+//        UIGraphicsEndImageContext()
+//        blurImageOverlay.image = screenshot
+        
 //        layoutCellViews()
     }
     
@@ -49,6 +69,8 @@ class LineupCardView: DraftboardNibView {
             cellView.heightRancor.constraintEqualToRancor(contentView.heightRancor, multiplier: 1.0 / 8.0).active = true
             cellView.centerXRancor.constraintEqualToRancor(contentView.centerXRancor).active = true
             
+            // this is the constraint for the first cell
+            // will need to be set to bottom of toggle
             if (previousCell == nil) {
                 cellView.topRancor.constraintEqualToRancor(contentView.topRancor).active = true
             }
