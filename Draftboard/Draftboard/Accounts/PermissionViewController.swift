1eada5945f9ac24b9502028ea53c7a6db1630a8e
Geof Crowl <geof@rallyinteractive.com>
2015-12-08 15:49:35 -0700

    Add xib and icons for permission priming

---
+++ Draftboard/Draftboard/Accounts/PermissionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PermissionViewController.swift
+//  Draftboard
+//
+//  Created by Geof Crowl on 12/7/15.
+//  Copyright © 2015 Rally Interactive. All rights reserved.
+//
+
+import UIKit
+
+class PermissionViewController: DraftboardViewController {
+
+    @IBOutlet weak var titleLabel: DraftboardLabel!
+    @IBOutlet weak var bigIconView: DraftboardImageView!
+    @IBOutlet weak var descriptionLabel: DraftboardLabel!
+    @IBOutlet weak var grantBtn: DraftboardButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        bigIconView.tintColor = bigIconView.tintColor
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
