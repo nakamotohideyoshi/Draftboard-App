de7580d24c8568c10eb6da61164eefbf0a2ea7a0
Anson Schall <anson@rallyinteractive.com>
2016-02-02 17:38:25 -0700

    Wrap a few more API endpoints with Data accessors

--- Draftboard/Draftboard/Data/Data.swift
+++ Draftboard/Draftboard/Data/Data.swift
@@ -80,10 +80,17 @@ private class Datum<T> {
 
 class Data {
     typealias LineupsPromise = Promise<([Lineup]?, Promise<[Lineup]>)>
+    typealias DraftGroupsPromise = Promise<([DraftGroup]?, Promise<[DraftGroup]>)>
     typealias DraftGroupPromise = Promise<(DraftGroup?, Promise<DraftGroup>)>
+    typealias ContestsPromise = Promise<([Contest]?, Promise<[Contest]>)>
 
-    // Upcoming lineups
+    // Storage
     private static var lineups: Datum<[Lineup]>?
+    private static var draftGroups: Datum<[DraftGroup]>?
+    private static var draftGroup = [Int: Datum<DraftGroup>]()
+    private static var contests: Datum<[Contest]>?
+
+    // Accessors
     class func lineups(maxCacheAge: NSTimeInterval = 60) -> LineupsPromise {
         if lineups == nil {
             lineups = Datum<[Lineup]> { API.lineupUpcoming() }
@@ -91,12 +98,24 @@ class Data {
         return lineups!.cachedAndFresh(maxCacheAge: maxCacheAge)
     }
     
-    // Upcoming draftgroups
-    private static var draftGroup = [Int: Datum<DraftGroup>]()
+    class func draftGroups(maxCacheAge: NSTimeInterval = 60) -> DraftGroupsPromise {
+        if draftGroups == nil {
+            draftGroups = Datum<[DraftGroup]> { API.draftGroupUpcoming() }
+        }
+        return draftGroups!.cachedAndFresh(maxCacheAge: maxCacheAge)
+    }
+    
     class func draftGroup(id id: Int, maxCacheAge: NSTimeInterval = 60) -> DraftGroupPromise {
         if draftGroup[id] == nil {
             draftGroup[id] = Datum<DraftGroup> { API.draftGroup(id: id) }
         }
         return draftGroup[id]!.cachedAndFresh(maxCacheAge: maxCacheAge)
+    }
+    
+    class func contests(maxCacheAge: NSTimeInterval = 60) -> ContestsPromise {
+        if contests == nil {
+            contests = Datum<[Contest]> { API.contestLobby() }
+        }
+        return contests!.cachedAndFresh(maxCacheAge: maxCacheAge)
     }
 }
