3c0c1e887e6d258391e318948ff8f375efeed041
Anson Schall <anson@rallyinteractive.com>
2016-07-15 23:49:11 -0600

    Show entries for lineups on back of lineup cards

--- Draftboard/Draftboard/Lineup/LineupEntryView.swift
+++ Draftboard/Draftboard/Lineup/LineupEntryView.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class LineupEntryView: UIView {
     
-    let tempLabel = UILabel()
+    let tableView = UITableView()
+    let headerView = UIView()
+    let footerView = LineupFooterView()
     
-    var tapAction: () -> Void = {}
+    // Header stuff
+    let sportIcon = UIImageView()
+    let nameLabel = UILabel()
+    let flipButton = UIButton()
+    let headerBorderView = UIView()
     
     init() {
         super.init(frame: CGRectZero)
@@ -28,7 +34,14 @@ class LineupEntryView: UIView {
     }
     
     override func layoutSubviews() {
-        tempLabel.frame = bounds
+        tableView.frame = bounds
+        headerView.frame = CGRectMake(0, 0, bounds.width, 68)
+        footerView.frame = CGRectMake(0, bounds.height - 68, bounds.width, 68)
+        
+        sportIcon.frame = CGRectMake(17, headerView.bounds.height * 0.5 - 13 * 0.5, 13, 13)
+        nameLabel.frame = CGRectInset(headerView.bounds, 44, 0)
+        flipButton.frame = CGRectMake(headerView.bounds.width - 44, 0, 44, headerView.bounds.height)
+        headerBorderView.frame = CGRectMake(0, headerView.bounds.height - 1, headerView.bounds.width, 1)
     }
     
     func setup() {
@@ -37,15 +50,52 @@ class LineupEntryView: UIView {
     }
     
     func addSubviews() {
-        addSubview(tempLabel)
+        addSubview(tableView)
+        addSubview(footerView)
+        addSubview(headerView)
+        
+        headerView.addSubview(sportIcon)
+        headerView.addSubview(nameLabel)
+        headerView.addSubview(flipButton)
+        headerView.addSubview(headerBorderView)
     }
     
     func otherSetup() {
-        backgroundColor = .whiteColor()
-        tempLabel.font = .openSans(size: 14.0)
-        tempLabel.textAlignment = .Center
-        tempLabel.textColor = .blackColor()
-        tempLabel.text = "Contest entries will go here!"
+        
+        tableView.backgroundColor = UIColor(0x4b545f)
+        tableView.separatorStyle = .None
+        headerView.backgroundColor = UIColor(0x4b545f)
+        headerView.alpha = 0.9
+        footerView.backgroundColor = UIColor(0x4b545f)
+        footerView.alpha = 0.9
+        footerView.userInteractionEnabled = false
+        
+        for subview in footerView.subviews {
+            if let statView = subview as? StatView {
+                if let countdownView = statView as? CountdownStatView {
+                    countdownView.countdownView.color = .whiteColor()
+                } else {
+                    statView.valueLabel.textColor = .whiteColor()
+                }
+                statView.rightBorderView.backgroundColor = UIColor(0x5f626d)
+            }
+        }
+        
+        sportIcon.image = UIImage(named: "icon-baseball")
+        sportIcon.tintColor = .whiteColor()
+        
+        flipButton.setImage(UIImage(named: "icon-flip-back"), forState: .Normal)
+        flipButton.imageEdgeInsets = UIEdgeInsetsMake(0, 0, 4, 4)
+        
+        nameLabel.font = UIFont.openSans(size: 16)
+        nameLabel.textColor = .whiteColor()
+        
+        headerBorderView.backgroundColor = UIColor(0x5f626d)
+        footerView.topBorderView.backgroundColor = UIColor(0x5f626d)
+
+        tableView.contentInset = UIEdgeInsetsMake(68, 0, 68, 0)
+        tableView.contentOffset = CGPointMake(0, -68)
+        tableView.scrollIndicatorInsets = tableView.contentInset
     }
     
 }
--- Draftboard/Draftboard/Lineup/LineupEntryViewController.swift
+++ Draftboard/Draftboard/Lineup/LineupEntryViewController.swift
@@ -11,25 +11,59 @@ import UIKit
 class LineupEntryViewController: DraftboardViewController {
     
     var lineupEntryView: LineupEntryView { return view as! LineupEntryView }
-    var lineup: Lineup?
-    var entries: [LineupEntry] = []
+    var tableView: UITableView { return lineupEntryView.tableView }
+    var flipButton: UIButton { return lineupEntryView.flipButton }
+    var sportIcon: UIImageView { return lineupEntryView.sportIcon }
+    var nameLabel: UILabel { return lineupEntryView.nameLabel }
+
+    var lineup: LineupWithStart? { didSet { viewDidLoad() } }
+    var entries: [LineupEntry] = [] { didSet { tableView.reloadData() } }
+    
     var flipAction: (() -> Void) = {}
 
-    convenience init(lineup: Lineup) {
-        self.init()
-        self.lineup = lineup
-    }
-    
     override func loadView() {
         self.view = LineupEntryView()
     }
     
     override func viewDidLoad() {
-        let tap = UITapGestureRecognizer(target: self, action: #selector(tapped))
-        view.addGestureRecognizer(tap)
+        // FIXME: Don't rely on lineup being non-nil
+        if lineup == nil {
+            let draftGroup = DraftGroup(id: 1, sportName: "nba", start: NSDate(), numGames: 0)
+            lineup = LineupWithStart(draftGroup: draftGroup)
+        }
+        
+        // Sport icon
+        sportIcon.image = UIImage(named: "icon-baseball")
+        
+        // Lineup name
+        nameLabel.text = lineup!.name
+        
+        // Edit button
+        
+        // Flip button
+        
+        // Players
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = 40
+//        tableView.rowHeight = 
+        
+        // Countdown
+        lineupEntryView.footerView.countdown.countdownView.date = lineup!.start
+        
+        lineup?.getEntries().then { entries -> Void in
+            self.entries = entries
+            self.tableView.reloadData()
+            let totalBuyin = entries.reduce(0) { $0 + $1.contest.buyin }
+            let feesText = Format.currency.stringFromNumber(totalBuyin)!
+            let text = "\(feesText) / \(entries.count)"
+            self.lineupEntryView.footerView.feesEntries.valueLabel.text = text
+        }
+        
+        flipButton.addTarget(self, action: #selector(flipButtonTapped), forControlEvents: .TouchUpInside)
     }
     
-    func tapped() {
+    func flipButtonTapped() {
         flipAction()
     }
 
@@ -45,16 +79,71 @@ extension TableViewDelegate: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let cell = LineupEntryCell()
         
         let entry = entries[indexPath.row]
-        let contestName = entry.contest.name
-        let fee = entry.contest.buyin
-        let action = {
-            entry.unregister()
-        }
+        cell.nameLabel.text = entry.contest.name
+        cell.feesLabel.text = Format.currency.stringFromNumber(entry.contest.buyin)!
+//        let action = {
+//            entry.unregister()
+//        }
         
         return cell
     }
     
+    // UITableViewDelegate
+    
+    func tableView(_: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
 }
+
+class LineupEntryCell: UITableViewCell {
+    
+    let nameLabel = UILabel()
+    let feesLabel = UILabel()
+    let borderView = UIView()
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setup()
+    }
+    
+    required convenience init?(coder: NSCoder) {
+        self.init()
+    }
+    
+    override func layoutSubviews() {
+        nameLabel.frame = CGRectMake(18, 0, bounds.width - 80, bounds.height)
+        feesLabel.frame = CGRectMake(bounds.width - 80, 0, 62, bounds.height)
+        borderView.frame = CGRectMake(0, bounds.height - 1, bounds.width, 1)
+    }
+
+    func setup() {
+        addSubviews()
+        setupSubviews()
+    }
+    
+    func addSubviews() {
+        contentView.addSubview(nameLabel)
+        contentView.addSubview(feesLabel)
+        contentView.addSubview(borderView)
+    }
+    
+    func setupSubviews() {
+        backgroundColor = .clearColor()
+        
+        nameLabel.font = .openSans(size: 11)
+        nameLabel.textColor = .whiteColor()
+        
+        feesLabel.font = UIFont.oswald(size: 12)
+        feesLabel.textColor = .whiteColor()
+        feesLabel.textAlignment = .Right
+
+        borderView.backgroundColor = UIColor(0x5f626d)
+    }
+
+}
+
+
