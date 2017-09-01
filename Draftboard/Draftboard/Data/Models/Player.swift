efefdc93ed14b8190948310ba9d354f1a4aeacf5
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2017-09-02 02:21:50 +0900

    update Player model

--- Draftboard/Draftboard/Data/Models/Player.swift
+++ Draftboard/Draftboard/Data/Models/Player.swift
@@ -25,14 +25,16 @@ class Player {
     let fppg: Double
     let teamAlias: String
     let srid: String
+    var stat: NSDictionary?
     
-    init(id: Int, name: String, salary: Double, fppg: Double, teamAlias: String, srid: String) {
+    init(id: Int, name: String, salary: Double, fppg: Double, teamAlias: String, srid: String, stat: NSDictionary? = nil) {
         self.id = id
         self.name = name
         self.salary = salary
         self.fppg = fppg
         self.teamAlias = teamAlias
         self.srid = srid
+        self.stat = stat
     }
 
     // Used by upcoming lineups API endpoint
@@ -46,7 +48,15 @@ class Player {
             let srid: String = try meta.get("srid")
             let team: NSDictionary = try meta.get("team")
             let teamAlias: String = try team.get("alias")
-            self.init(id: id, name: name, salary: salary, fppg: fppg, teamAlias: teamAlias, srid: srid)
+            let player_stats: [NSDictionary] = try json.get("player_stats")
+            let player_stat: NSDictionary?
+            
+            if player_stats.count > 0 {
+                player_stat = player_stats[0]
+            } else {
+                player_stat = nil
+            }
+            self.init(id: id, name: name, salary: salary, fppg: fppg, teamAlias: teamAlias, srid: srid, stat: player_stat)
         } catch let error {
             throw APIError.ModelError(self.dynamicType, error)
         }
@@ -57,10 +67,10 @@ class PlayerWithPosition: Player, HasPosition {
     let position: String
     let teamSRID: String
     
-    init(id: Int, name: String, salary: Double, fppg: Double, teamAlias: String, srid: String, position: String, teamSRID: String) {
+    init(id: Int, name: String, salary: Double, fppg: Double, teamAlias: String, srid: String, stat: NSDictionary?, position: String, teamSRID: String) {
         self.position = position
         self.teamSRID = teamSRID
-        super.init(id: id, name: name, salary: salary, fppg: fppg, teamAlias: teamAlias, srid: srid)
+        super.init(id: id, name: name, salary: salary, fppg: fppg, teamAlias: teamAlias, srid: srid, stat: stat)
     }
     
     // Used by draftgroup id API endpoint
@@ -74,7 +84,7 @@ class PlayerWithPosition: Player, HasPosition {
             let teamAlias: String = try json.get("team_alias")
             let teamSRID: String = try json.get("team_srid")
             let position: String = try json.get("position")
-            self.init(id: id, name: name, salary: salary, fppg: fppg, teamAlias: teamAlias, srid: srid, position: position, teamSRID: teamSRID)
+            self.init(id: id, name: name, salary: salary, fppg: fppg, teamAlias: teamAlias, srid: srid, stat: nil, position: position, teamSRID: teamSRID)
         } catch let error {
             throw APIError.ModelError(self.dynamicType, error)
         }
@@ -85,16 +95,16 @@ class PlayerWithPositionAndGame: PlayerWithPosition, HasGame {
     let team: Team
     let game: Game
     
-    init(id: Int, name: String, salary: Double, fppg: Double, teamAlias: String, srid: String, position: String, teamSRID: String, game: Game) {
+    init(id: Int, name: String, salary: Double, fppg: Double, teamAlias: String, srid: String, stat: NSDictionary?, position: String, teamSRID: String, game: Game) {
         self.team = (teamAlias == game.home.alias) ? game.home : game.away
         self.game = game
-        super.init(id: id, name: name, salary: salary, fppg: fppg, teamAlias: teamAlias, srid: srid, position: position, teamSRID: teamSRID)
+        super.init(id: id, name: name, salary: salary, fppg: fppg, teamAlias: teamAlias, srid: srid, stat: stat, position: position, teamSRID: teamSRID)
     }
 }
 
 extension PlayerWithPosition {
     func withGame(game: Game) -> PlayerWithPositionAndGame {
-        return PlayerWithPositionAndGame(id: id, name: name, salary: salary, fppg: fppg, teamAlias: teamAlias, srid: srid, position: position, teamSRID: teamSRID, game: game)
+        return PlayerWithPositionAndGame(id: id, name: name, salary: salary, fppg: fppg, teamAlias: teamAlias, srid: srid, stat: stat, position: position, teamSRID: teamSRID, game: game)
     }
 }
 
